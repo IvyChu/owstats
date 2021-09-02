af3969e025ebda27c615f9c924b5259f20413501
IvyChu <ivanica@gmail.com>
2021-09-02 21:02:09 +0200

    Added seasons to plots

--- owstats/utils.py
+++ owstats/utils.py
@@ -1,10 +1,10 @@
 import os
-from datetime import date, datetime, timedelta
+from datetime import date, datetime
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import requests
-from flask import current_app
+
 
 BASE_URL = 'https://ow-api.com/v1/stats'
 
@@ -43,31 +43,35 @@ def is_it_new_season(next_switch_date):
     return next_switch_date < now
 
 
-def make_plot(user):
+def make_plot(user, season=0):
     data = {'Games played': [],
         'Tank SR': [],
         'Damage SR': [],
         'Support SR': []
        }
+    
+    if season == 0:
+        season = user.comp_stats[0].season
 
     for stat in user.comp_stats:
-        data['Games played'].append(stat.games_played)
-        data['Tank SR'].append(stat.rating_tank)
-        data['Damage SR'].append(stat.rating_damage)
-        data['Support SR'].append(stat.rating_support)
+        if stat.season == season:
+            data['Games played'].append(stat.games_played)
+            data['Tank SR'].append(stat.rating_tank)
+            data['Damage SR'].append(stat.rating_damage)
+            data['Support SR'].append(stat.rating_support)
     
     df = pd.DataFrame(data,columns=['Games played','Tank SR','Damage SR','Support SR'])
     
     plt.plot(df['Games played'], df['Tank SR'], '.:b', label='Tank SR')
     plt.plot(df['Games played'], df['Damage SR'], '*:r', label='Damage SR')
     plt.plot(df['Games played'], df['Support SR'], '^:c', label='Support SR')
-    plt.title('SR changes per role', loc = 'left')
+    plt.title(f'Season {stat.season}: SR changes per role', loc = 'left')
     plt.xlabel('Games played')
     plt.ylabel('SR')
     plt.legend()
     plt.grid(axis = 'y')
     # plt.show()
-    plot_fn = f"{user.username}_{user.platform}_{user.region}.png"
+    plot_fn = f"{user.username}_{user.platform}_{user.region}_{season}.png"
     plot_dir = os.path.dirname(__loader__.path)
     plot_path = os.path.join(plot_dir, 'static/plots', plot_fn)
     plt.savefig(plot_path, dpi=100)
